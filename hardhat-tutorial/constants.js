66557813ccc7f3fa5be6908c67c5a008a22624bb
Gayathri Krishnan <gayathri.krishnan.designer@gmail.com>
2023-06-21 19:31:44 +0530

    Initial commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,6 @@
+
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x2F586c5417482c8E44f311221B8fe0881b699027";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
